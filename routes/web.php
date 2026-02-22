74cc996303cf73bf34ed50821ad82cde7387d46c
rafaliefatah <rafazian336@gmail.com>
2026-02-22 15:23:33 +0700

    JS2_basic routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 244107020100 <br> Nama: Rafazian Alief Fatah';
 });

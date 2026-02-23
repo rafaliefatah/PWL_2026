04990dee5eeca9e40d59ba3433f1cef0c581106f
rafaliefatah <rafazian336@gmail.com>
2026-02-23 12:36:17 +0700

    JS2_membuat view

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,7 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
  'create', 'store', 'update', 'destroy'
 ]);
+
+Route::get('/greeting', function () {
+    return view('hello', ['name' => 'Rafa']);
+});

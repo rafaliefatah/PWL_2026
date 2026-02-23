bbe9d67d06c052a89f98a91e60caf76e08c8cf17
rafaliefatah <rafazian336@gmail.com>
2026-02-23 12:49:17 +0700

    JS2_view dari controller

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,4 @@ Route::resource('photos', PhotoController::class)->except([
  'create', 'store', 'update', 'destroy'
 ]);
 
-Route::get('/greeting', function () {
-    return view('blog.hello', ['name' => 'Rafa']);
-});
+Route::get('/greeting', [WelcomeController::class,'greeting']);
